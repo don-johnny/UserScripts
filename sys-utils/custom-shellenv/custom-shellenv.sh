dd247bed8411668c96cb6e4c551af406ef953167
tinkernels <don.johnny.cn@gmail.com>
2022-12-16 07:21:55 +0800

    Change files layout.

--- sys-utils/custom-shellenv/custom-shellenv.sh
+++ sys-utils/custom-shellenv/custom-shellenv.sh
@@ -6,16 +6,16 @@ else
     git clone --depth 1 "https://github.com/tmux-plugins/tpm.git" "$HOME/.tmux/plugins/tpm"
 fi
 
-curl -sSfLo "$HOME/.tmux.conf" "https://raw.githubusercontent.com/tinkernels/userscripts/gh-pages/_tmux.conf"
+curl -sSfLo "$HOME/.tmux.conf" "https://raw.githubusercontent.com/tinkernels/userscripts/gh-pages/sys-utils/custom-shellenv/custom-tmux.conf"
 
 if [ ! -d "$HOME/.oh-my-zsh" ];then
     curl -sSfL "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh" | bash -s
 fi
 
-curl -sSfLo "$HOME/.profile" "https://raw.githubusercontent.com/tinkernels/userscripts/gh-pages/_shellprofile"
+curl -sSfLo "$HOME/.profile" "https://raw.githubusercontent.com/tinkernels/userscripts/gh-pages/sys-utils/custom-shellenv/custom-shellprofile"
 (cd "$HOME" && ln -sf .profile .zprofile && ln -sf .profile .bash_profile)
 
-curl -sSfLo "$HOME/.shellrc" "https://raw.githubusercontent.com/tinkernels/userscripts/gh-pages/_shellrc"
+curl -sSfLo "$HOME/.shellrc" "https://raw.githubusercontent.com/tinkernels/userscripts/gh-pages/sys-utils/custom-shellenv/custom-shellrc"
 for F in "$HOME/.zshrc" "$HOME/.bashrc"
 do
     grep -q "source \$HOME/.shellrc" "$F" || echo "source \$HOME/.shellrc" >> "$F"
