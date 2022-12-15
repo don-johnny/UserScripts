997a6578c4c4aaa005a5506c593fba6827e870ab
tinkernels <don.johnny.cn@gmail.com>
2022-12-16 07:10:59 +0800

    Change remote file links.

--- custom_shellenv.sh
+++ custom_shellenv.sh
@@ -6,16 +6,16 @@ else
     git clone --depth 1 "https://github.com/tmux-plugins/tpm.git" "$HOME/.tmux/plugins/tpm"
 fi
 
-curl -sSfLo "$HOME/.tmux.conf" "https://tinkernels.github.io/userscripts/_tmux.conf"
+curl -sSfLo "$HOME/.tmux.conf" "https://raw.githubusercontent.com/tinkernels/userscripts/gh-pages/_tmux.conf"
 
 if [ ! -d "$HOME/.oh-my-zsh" ];then
     curl -sSfL "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh" | bash -s
 fi
 
-curl -sSfLo "$HOME/.profile" "https://tinkernels.github.io/userscripts/_shellprofile"
+curl -sSfLo "$HOME/.profile" "https://raw.githubusercontent.com/tinkernels/userscripts/gh-pages/_shellprofile"
 (cd "$HOME" && ln -sf .profile .zprofile && ln -sf .profile .bash_profile)
 
-curl -sSfLo "$HOME/.shellrc" "https://tinkernels.github.io/userscripts/_shellrc"
+curl -sSfLo "$HOME/.shellrc" "https://raw.githubusercontent.com/tinkernels/userscripts/gh-pages/_shellrc"
 for F in "$HOME/.zshrc" "$HOME/.bashrc"
 do
     grep -q "source \$HOME/.shellrc" "$F" || echo "source \$HOME/.shellrc" >> "$F"
